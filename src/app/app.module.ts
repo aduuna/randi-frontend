d9b5029a161d0a59ff609bfea7374dd4b58bdf05
Vincent Aduuna Frimpong <tossia13@gmail.com>
2019-06-20 02:58:19 +0000

    modified root component to handle routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { ProjectService } from './project.service'
+import { AuthService } from './auth.service';
+import { HomeComponent } from './home/home.component'
+import { AuthGuard } from './auth.guard'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    ProjectListComponent,
+    ProjectDetailComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'projects',
+        component: ProjectListComponent,
+        canActivate: [AuthGuard]
+      },
+      {
+        path: 'detail',
+        component: ProjectDetailComponent,
+        canActivate: [AuthGuard]
+      }
+    ]),
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [ProjectService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
