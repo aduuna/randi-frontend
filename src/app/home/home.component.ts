8b415f840c48c3dd3165ed1b7d1ec07c09794f17
Vincent Aduuna Frimpong <tossia13@gmail.com>
2019-06-20 02:47:20 +0000

    added home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  public status;
+
+  constructor(private _authService: AuthService) { }
+
+  ngOnInit() {
+    if(this._authService.isLoggedIn){
+      this.status = true;
+    }
+  }
+
+}
