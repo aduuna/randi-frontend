0906eb0c099f35375737def1890aa6942da0f9a8
Vincent Aduuna Frimpong <tossia13@gmail.com>
2019-06-20 02:51:50 +0000

    working authentication

---
+++ src/app/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http'
+import { User } from  './user'
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class AuthService {
+
+  private token;
+
+  constructor(private http: HttpClient) { }
+
+  loginUser(UserInfo: User): Observable<Object | void> {
+    const BASE_URL = "https://randi-api-test.herokuapp.com/api/v1/";
+    let url = BASE_URL + 'login/'
+    let params = {
+      email: UserInfo.username,
+      password: UserInfo.password
+    }
+    return this.http.post(url, params)
+  }
+
+  public login(userInfo: User){
+    localStorage.setItem('token', this.token);
+  }
+
+  public isLoggedIn(){
+    return localStorage.getItem('token') !== null;
+
+  }
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from  '@angular/forms';
+import { Router } from  '@angular/router';
+import { User } from  '../user';
+import { AuthService } from  '../auth.service'
+
+@Component({
+  selector: '.app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  isSubmitted  =  false;
+  failed = false;
+  constructor(private _authService: AuthService, private router: Router, private formBuilder: FormBuilder ) { }
+
+  ngOnInit() {
+    this.loginForm  =  this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  get formControls() { return this.loginForm.controls; }
+  flush(){ this.failed=false}
+
+  login(){
+    console.log(this.loginForm.value);
+    this.isSubmitted = true;
+    if(this.loginForm.invalid){
+      return;
+    }
+    console.log(this.loginForm.value)
+    this._authService.loginUser(this.loginForm.value)
+    .subscribe(
+      (res) => {
+        localStorage.setItem('user', res['user'].toString());
+        localStorage.setItem('token', res['token'])
+      },
+      (err)=>{this.failed = true},
+      ()=>{this.router.navigateByUrl('')}
+    )
+  }
+
+}
